cfadd168913d651b1817557498dac5bf3896d756
Sergei Monakhov <sergey.monakhov@gmail.com>
2025-10-22 13:17:37 +0300

    add run tests on ci

---
+++ kube_models_generator.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+K8S_REPO_URL="${K8S_REPO_URL:-https://github.com/kubernetes/kubernetes}"
+K8S_OPENAPI_SPEC_PATH="${K8S_OPENAPI_SPEC_PATH:-api/openapi-spec/v3}"
+K8S_VERSIONS="${K8S_VERSIONS:-1.23 1.24 1.25 1.26 1.27 1.28 1.29 1.30 1.31 1.32 1.33 1.34}"
+
+OPENAPI_SPEC_DIR="${OPENAPI_SPEC_DIR:-./kube_openapi_spec}"
+DATA_MODEL_DIR="${DATA_MODEL_DIR:-./kube_models}"
+
+mkdir -p "${OPENAPI_SPEC_DIR}"
+
+download_version() {
+    local K8S_VERSION=$1
+    local REPO_URL=$2
+    local SPEC_DIR=$3
+    local SPEC_PATH=$4
+    
+    local K8S_VERSION_DIR="${SPEC_DIR}/${K8S_VERSION}"
+    
+    git clone \
+        --depth 1 \
+        --filter=blob:none \
+        --sparse \
+        --branch "release-${K8S_VERSION}" \
+        "${REPO_URL}" \
+        "${K8S_VERSION_DIR}"
+    
+    cd "${K8S_VERSION_DIR}"
+    git sparse-checkout set "${SPEC_PATH}"
+    cd - > /dev/null
+}
+
+echo "=== Downloading k8s openapi specs ==="
+MAX_PARALLEL_DOWNLOAD=${MAX_PARALLEL_DOWNLOAD:-5}
+COUNT=0
+
+for K8S_VERSION in ${K8S_VERSIONS}; do
+    download_version "${K8S_VERSION}" "${K8S_REPO_URL}" "${OPENAPI_SPEC_DIR}" "${K8S_OPENAPI_SPEC_PATH}" &
+    
+    ((COUNT++))
+    if (( COUNT >= MAX_PARALLEL_DOWNLOAD )); then
+        wait -n
+        ((COUNT--))
+    fi
+done
+wait
+
+echo "=== Generating data models ==="
+for K8S_VERSION in ${K8S_VERSIONS}; do
+    K8S_VERSION_DIR="${OPENAPI_SPEC_DIR}/${K8S_VERSION}"
+    
+    echo "Generate python Data Model for k8s version: ${K8S_VERSION}"
+    uv run packages/kubesdk_cli/src/kubesdk_cli/cli.py \
+        --from-dir "${K8S_VERSION_DIR}/${K8S_OPENAPI_SPEC_PATH}" \
+        --output "${DATA_MODEL_DIR}"
+done
