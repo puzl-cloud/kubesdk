06531c8ee0ed7e0a781fc92b46f2be241e6c8b0e
Sergei Monakhov <sergey.monakhov@gmail.com>
2025-10-27 12:40:19 +0200

    update run tests

--- kube_models_generator.sh
+++ kube_models_generator.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 K8S_REPO_URL="${K8S_REPO_URL:-https://github.com/kubernetes/kubernetes}"
 K8S_OPENAPI_SPEC_PATH="${K8S_OPENAPI_SPEC_PATH:-api/openapi-spec/v3}"
 K8S_VERSIONS="${K8S_VERSIONS:-1.23 1.24 1.25 1.26 1.27 1.28 1.29 1.30 1.31 1.32 1.33 1.34}"
 
-OPENAPI_SPEC_DIR="${OPENAPI_SPEC_DIR:-./kube_openapi_spec}"
-DATA_MODEL_DIR="${DATA_MODEL_DIR:-./kube_models}"
+OPENAPI_SPEC_DIR="${OPENAPI_SPEC_DIR:-.run/kube_openapi_spec}"
+DATA_MODEL_DIR="${DATA_MODEL_DIR:-packages/kube_models/src/kube_models}"
 
 mkdir -p "${OPENAPI_SPEC_DIR}"
 
@@ -16,7 +16,12 @@ download_version() {
     local SPEC_PATH=$4
     
     local K8S_VERSION_DIR="${SPEC_DIR}/${K8S_VERSION}"
-    
+
+    if [ -d "${K8S_VERSION_DIR}" ]; then
+        echo "Directory ${K8S_VERSION_DIR} already exists, skipping"
+        return 0
+    fi
+
     git clone \
         --depth 1 \
         --filter=blob:none \
@@ -25,9 +30,9 @@ download_version() {
         "${REPO_URL}" \
         "${K8S_VERSION_DIR}"
     
-    cd "${K8S_VERSION_DIR}"
+    cd "${K8S_VERSION_DIR}" || exit
     git sparse-checkout set "${SPEC_PATH}"
-    cd - > /dev/null
+    cd - > /dev/null || exit
 }
 
 echo "=== Downloading k8s openapi specs ==="
